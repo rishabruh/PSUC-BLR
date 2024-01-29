e12d1fa2fdfe0f47d107bf69713e2e7957e025c8
rishabruh <rishabh.agg.2005@gmail.com>
2024-01-29 15:47:25 +0530

    Fixed "%" sign display in discount.c

--- discount.c
+++ discount.c
@@ -11,30 +11,30 @@ void main(){
     if (profPerc>80){
       disc=0.18*sp;
       newsp=sp-disc;
-      printf("18 percent discount applied, item costs %f\n",newsp);
+      printf("18 %% discount applied, item costs %f\n",newsp);
     }
     else if (profPerc>=56 && profPerc<=80){
         disc=0.14*sp;
         newsp=sp-disc;
-        printf("14 percent discount applied, item costs %f\n",newsp);
+        printf("14 %% discount applied, item costs %f\n",newsp);
       }
     
     else if (profPerc>=36 && profPerc<=55){
         disc=0.12*sp;
         newsp=sp-disc;
-        printf("12 percent discount applied, item costs %f\n",newsp);
+        printf("12 %% discount applied, item costs %f\n",newsp);
     }
       
     else if (profPerc>=20 && profPerc<=35){
         disc=0.08*sp;
         newsp=sp-disc;
-        printf("8 percent discount applied, item costs %f\n",newsp);
+        printf("8 %% discount applied, item costs %f\n",newsp);
       }
 
     else{ 
         disc=0;
         newsp=sp-disc;
-        printf("0 percent discount applied, item costs %f\n",newsp);
+        printf("0 %% discount applied, item costs %f\n",newsp);
     }
   }
 }
